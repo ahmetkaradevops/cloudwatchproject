8f40495c4be7c003672e891ecffc97f89680f59b
ahmetkaradevopsaws <ahmetkaradevopsaws@gmail.com>
2022-10-09 00:36:49 +0200

    sns_sqs.tf, sqs_policy.json and sqs-receive-policy.json files created. The files automotically send a sms when the Windows Ec2 shut-down

--- sns_sqs.tf
+++ sns_sqs.tf
@@ -6,18 +6,15 @@ resource "aws_sqs_queue" "my_lambda_restart_queue" {
   receive_wait_time_seconds = 0
   visibility_timeout_seconds = 2400
 }
-
 # Topic and Subscription
 resource "aws_sns_topic" "sns_my_lambda_restart_topic" {
   name = "my_lambda_restart"
 }
 resource "aws_sns_topic_subscription" "sns_my_lambda_restart_sub" {
   topic_arn = "${aws_sns_topic.sns_my_lambda_restart_topic.arn}"
-  protocol  = "sqs"
-  endpoint  = "${aws_sqs_queue.my_lambda_restart_queue.arn}"
-  raw_message_delivery = true
+  protocol  = "email"
+  endpoint  = "ahmetkaradevopsaws@gmail.com"
 }
-
 # Policy
 data "template_file" "sqs-policy" {
   template =  "${file("~/Desktop/try/sqs_policy.json")}"
@@ -26,7 +23,6 @@ data "template_file" "sqs-policy" {
     sns_arn = "${aws_sns_topic.sns_my_lambda_restart_topic.arn}"
   }
 }
-
 resource "aws_sqs_queue_policy" "my-lambda-restart-sqs-policy" {
   queue_url = "${aws_sqs_queue.my_lambda_restart_queue.id}"
   policy = "${data.template_file.sqs-receive-policy.rendered}"
